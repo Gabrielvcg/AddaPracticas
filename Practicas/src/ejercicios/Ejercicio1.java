e14155e4123cfa587b728a8c1fd885613930acb4
Gabriel <gabrivacaro@gmail.com>
2023-10-04 18:30:19 +0200

    Ejercicio 1 finalizado y test completo

--- Practicas/src/ejercicios/Ejercicio1.java
+++ Practicas/src/ejercicios/Ejercicio1.java
@@ -8,59 +8,43 @@ import util.EnteroCadena;
 
 public class Ejercicio1 {
 
-	
-	public static String ejercicio1 (Integer varA, Integer varB) {
-		UnaryOperator<EnteroCadena> nx = elem ->
-		{
-		return EnteroCadena.of(elem.a()+3,
-		elem.a()%2==0?
-		elem.a()+"*":
-		elem.a()+"!");
+	public static String ejercicio1(Integer varA, Integer varB) {
+		UnaryOperator<EnteroCadena> nx = elem -> {
+			return EnteroCadena.of(elem.a() + 3, elem.a() % 2 == 0 ? elem.a() + "*" : elem.a() + "!");
 		};
-		return Stream
-		.iterate(EnteroCadena.of(varA,"A"), elem -> elem.a() < varB, nx)
-		.filter(elem -> elem.a()%10 != 0)
-		.map(elem -> elem.s())
-		.collect(Collectors.joining("-"));
-		}
-	
+		return Stream.iterate(EnteroCadena.of(varA, "A"), elem -> elem.a() < varB, nx)
+				.filter(elem -> elem.a() % 10 != 0).map(elem -> elem.s()).collect(Collectors.joining("-"));
+	}
+
 	public static String ejercicio1Iterativo(Integer varA, Integer varB) {
-		UnaryOperator<EnteroCadena> nx = elem ->
-		{
-		return EnteroCadena.of(elem.a()+3,
-		elem.a()%2==0?
-		elem.a()+"*":
-		elem.a()+"!");
+		UnaryOperator<EnteroCadena> nx = elem -> {
+			return EnteroCadena.of(elem.a() + 3, elem.a() % 2 == 0 ? elem.a() + "*" : elem.a() + "!");
 		};
-		
+
 		EnteroCadena e = EnteroCadena.of(varA, "A");
 		String result = "";
 
 		while (e.a() < varB) {
 			if (e.a() % 10 != 0) {
-				if (result.length()>0) {
-					result=result.concat("-");
+				if (result.length() > 0) {
+					result = result.concat("-");
 				}
-					result=result.concat(e.s());
+				result = result.concat(e.s());
 			}
 			e = nx.apply(e);
 		}
-		
+
 		return result;
 	}
-	
+
 	public static String ejercicio1Recursivo(Integer varA, Integer varB) {
-		
-		UnaryOperator<EnteroCadena> nx = elem ->
-		{
-		return EnteroCadena.of(elem.a()+3,
-		elem.a()%2==0?
-		elem.a()+"*":
-		elem.a()+"!");
+
+		UnaryOperator<EnteroCadena> nx = elem -> {
+			return EnteroCadena.of(elem.a() + 3, elem.a() % 2 == 0 ? elem.a() + "*" : elem.a() + "!");
 		};
-		
+
 		return ejercicio1RecursivoAux(nx, varA, varB, EnteroCadena.of(varA, "A"), "");
-		
+
 	}
 
 	public static String ejercicio1RecursivoAux(UnaryOperator<EnteroCadena> nx, Integer varA, Integer varB,
--- Practicas/src/tests/TestEjercicio1.java
+++ Practicas/src/tests/TestEjercicio1.java
@@ -11,12 +11,14 @@ public class TestEjercicio1 {
             String archivoEntrada = "../Practicas/ficheros/PI1Ej1DatosEntrada.txt";
 
             List<String> lineas = Files2.linesFromFile(archivoEntrada);
-			        
-			String[] valores =lineas.get(1).split(",");
+			for (String linea: lineas) {
+			String[] valores =linea.split(",");
 			Integer varA = Integer.parseInt(valores[0]);
-			Integer varB = Integer.parseInt(valores[1]);       
+			Integer varB = Integer.parseInt(valores[1]);   
+			
+            System.out.println("Test con valores " + linea );
             
-            // Llamar a tus métodos con los valores leídos
+			// Llamar a tus métodos con los valores leídos
             String resultadoFuncional = Ejercicio1.ejercicio1(varA, varB);
             String resultadoIterativo = Ejercicio1.ejercicio1Iterativo(varA, varB);
             String resultadoRecursivo = Ejercicio1.ejercicio1Recursivo(varA, varB);
@@ -25,8 +27,9 @@ public class TestEjercicio1 {
             System.out.println("Resultado Funcional: " + resultadoFuncional);
             System.out.println("Resultado Iterativo: " + resultadoIterativo);
             System.out.println("Resultado Recursivo: " + resultadoRecursivo);
-
-        
+            System.out.println();
+			}
+           
        
     }
 	}
