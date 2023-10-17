df88db96981df63563d64e6b207fd76db7f3d8b3
gabrielvcg <gabrivacaro@gmail.com>
2023-10-17 09:58:06 +0200

    Ejercicio 4 realizado a excepción de su test y comprobaciones

---
+++ Practicas/src/ejercicios/Ejercicio4.java
@@ -0,0 +1,69 @@
+package ejercicios;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import us.lsi.common.IntPair;
+
+public class Ejercicio4 {
+	
+	public static String ejercicio4RecursivoSinMemoria(Integer a, Integer b) { // Definicion del enunciado
+		String r = null;	
+		if (a <= 4) {
+			r=(a.toString()+"."+b.toString());
+		} else if(b <= 4) {
+
+			r=(b.toString()+"-"+a.toString());
+
+		}else {
+			r = ejercicio4RecursivoSinMemoria(a/2, b-2).toString() +","+ 
+					ejercicio4RecursivoSinMemoria(a-2, b/2)+","+ejercicio4RecursivoSinMemoria(a-1, b-1);
+
+		}
+		return r;
+	}
+
+	public static String ejercicio4RecursivaConMemoria(Integer a, Integer b) {
+		return gRecConMem(a, b, new HashMap<>());
+	}
+	private static String gRecConMem(Integer a, Integer b, Map<IntPair, String> m) {
+		String r = null;
+		IntPair key = IntPair.of(a, b);
+		if(m.containsKey(key)) {
+			r = m.get(key);
+		} else {
+			if (a <= 4) {
+				r=(a.toString()+"."+b.toString());
+			} else if(b <= 4) {
+
+				r=(b.toString()+"-"+a.toString());
+
+			}else {
+				r = gRecConMem(a/2, b-2,m).toString() +","+ 
+						gRecConMem(a-2, b/2,m)+","+gRecConMem(a-1, b-1,m);
+		
+		}
+			m.put(IntPair.of(a, b), r);
+		}
+		return r;
+	}
+
+	public static String ejercicio4Iterativo(Integer a, Integer b) {
+		Map<IntPair, String> map = new HashMap<>();
+		
+		String v = null;
+		for (Integer i = 0; i <= a; i++) {
+			for (Integer j = 0; j <= b; j++) {
+				if (i <= 4) {
+					v = i.toString()+"."+j.toString();
+				}else if (j <= 4) {
+					v = j.toString()+"-"+i.toString();
+				} else {
+					v = map.get(IntPair.of(i/2, j-2)) +","+ map.get(IntPair.of(i-3, j/2))+","+ map.get(IntPair.of(i-1, j-1));
+				}
+				map.put(IntPair.of(i, j), v);
+			}
+		}
+		return map.get(IntPair.of(a, b));
+	}
+}
---
+++ Practicas/src/tests/TestEjercicio4.java
@@ -0,0 +1,39 @@
+package tests;
+
+import java.util.List;
+
+import ejercicios.Ejercicio4;
+import us.lsi.common.Files2;
+
+public class TestEjercicio4 {
+public static void main(String[] args) {
+		
+        // Ruta del archivo de entrada
+        String archivoEntrada = "../Practicas/ficheros/PI1Ej4DatosEntrada.txt";
+
+        List<String> lineas = Files2.linesFromFile(archivoEntrada);
+		for (String linea: lineas) {
+		String[] valores =linea.split(",");
+		Integer a = Integer.parseInt(valores[0]);
+		Integer b = Integer.parseInt(valores[1]);   
+		
+        System.out.println("Test con valores " + linea );
+        
+        String resultadoRecursivoSinMemoria = Ejercicio4.ejercicio4RecursivoSinMemoria(a, b);
+        String resultadoRecursivoConMemoria = Ejercicio4.ejercicio4RecursivaConMemoria(a, b);
+        String resultadoIterativo = Ejercicio4.ejercicio4Iterativo(a, b);
+
+        // Imprimir los resultados
+       // System.out.println("Resultado Funcional: " + resultadoFuncional);
+        //System.out.println("Resultado Iterativo: " + resultadoIterativo);
+        System.out.println("Resultado Recursivo sin memoria: " + resultadoRecursivoSinMemoria);
+        System.out.println("Resultado Recursivo final con memoria: " + resultadoRecursivoConMemoria);
+        System.out.println("Resultado Iterativo: " + resultadoIterativo);
+      
+
+        System.out.println();
+		}
+       
+   
+}
+}
