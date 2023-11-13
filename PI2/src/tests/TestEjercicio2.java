4b0e38026b150fd9cd2b348e6ac995f9d257b158
gabrielvcg <gabrivacaro@gmail.com>
2023-11-13 16:37:31 +0100

    Ejercicio 2 funcional

--- PI2/src/tests/TestEjercicio2.java
+++ PI2/src/tests/TestEjercicio2.java
@@ -2,38 +2,45 @@ package tests;
 
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
 import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.Random;
 
 import org.apache.commons.math3.fitting.WeightedObservedPoint;
 
-import us.lsi.common.List2;
 import us.lsi.common.Pair;
 import us.lsi.curvefitting.DataFile;
 import us.lsi.curvefitting.Fit;
 import us.lsi.curvefitting.GenData;
 import us.lsi.curvefitting.PowerLog;
 import us.lsi.graphics.MatPlotLib;
-import us.lsi.streams.Collectors2;
-import us.lsi.recursivos.problemasdelistas;
-
+import us.lsi.math.Math2;
+import us.lsi.recursivos.problemasdelistas.ProblemasDeListas;
 
 
 public class TestEjercicio2 {
 	
-	private static Integer nMin = 100; // n mínimo para el cálculo 
+	private static Integer nMin = 1000; // n mínimo para el cálculo 
 	private static Integer nMax = 10000; // n máximo para el cálculo 
-	private static Integer nIncr = 100; // incremento en los valores de n del cálculo 
+	private static Integer nIncr = 530; // incremento en los valores de n del cálculo 
 	private static Integer nIter = 50; // número de iteraciones para cada medición de tiempo
 	private static Integer nIterWarmup = 1000; // número de iteraciones para warmup
 	
+	public static List<Integer> cargarLista(Integer t) {
+		Integer i=0;
+		List<Integer> ac=new ArrayList<Integer>();
+		while(i<t) {
+			ac.add(Math2.getRandom().nextInt());
+			i++;
+		}
+		return ac;
+	}
 
 	public static void genDataE2() {
+		
+		
 		String file = "ficheros_generados/E2.txt";
-		Function<Integer,Long> f1 = GenData.time(t -> Collectors2.mergeSort(new ArrayList<Integer>(),Comparator.naturalOrder(),t));
+		Function<Integer,Long> f1 = GenData.time(t -> ProblemasDeListas.mergeSort(cargarLista(t),7000));
 //		Integer tMin,Integer tMax,Integer tInc,Integer numIter,Integer numIterWarmup
 		GenData.tiemposEjecucionAritmetica(f1,file,nMin,nMax,nIncr,nIter,nIterWarmup);
 	}
@@ -46,15 +53,7 @@ public class TestEjercicio2 {
 		System.out.println(pl.getEvaluation().getRMS());
 		MatPlotLib.show(file, pl.getFunction(), pl.getExpression());
 	}
-	 private static List<Integer> generarListaAleatoria(int tamano) {
-	        return new Random().ints(tamano).boxed().collect(Collectors.toList());
-	    }
-	 
-	 public static <E extends Comparable<? super E>> void mergeSort(List<E> lista, Integer umbral){
-			Comparator<? super E> ord = Comparator.naturalOrder();
-			List<E> ls = List2.ofCollection(lista);
-			mgSort(lista,0,lista.size(),ord,ls,umbral);	
-		}
+	
 		
 	 
 	public static void main(String[] args) {
