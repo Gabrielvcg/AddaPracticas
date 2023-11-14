e0ad14c61b7a36c656d25fe439fe9fe5cc6bd68f
gabrielvcg <gabrivacaro@gmail.com>
2023-11-14 13:39:45 +0100

    prueba de commit

--- PI2/src/tests/TestEjercicio3.java
+++ PI2/src/tests/TestEjercicio3.java
@@ -4,7 +4,7 @@ public class TestEjercicio3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("su noma");
+		System.out.println("su no2ma");
 	}
 
 }
